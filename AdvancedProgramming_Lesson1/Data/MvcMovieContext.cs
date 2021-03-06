3568b81d940cb71b3fd00709613bc58d4bba78ba
leslaw pawlaczyk <l.pawlaczyk@cognitum.eu>
2021-03-06 13:47:01 +0100

    First draft of Movies Database

---
+++ AdvancedProgramming_Lesson1/Data/MvcMovieContext.cs
@@ -0,0 +1,14 @@
+﻿using AdvancedProgramming_Lesson1.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace AdvancedProgramming_Lesson1.Data
+{
+    public class MvcMovieContext : DbContext
+    {
+        public MvcMovieContext(DbContextOptions<MvcMovieContext> options)
+        : base(options)
+        {
+        }
+        public DbSet<Movie> Movie { get; set; }
+    }
+}
---
+++ AdvancedProgramming_Lesson1/README.md
@@ -0,0 +1,12 @@
+# How to run
+
+To run compile and the in Tools -> Nuget Package Manager Console issue the following:
+
+```
+Add-Migration InitialCreate
+Update-Database
+```
+
+
+# Last modified
+2021/03/06
